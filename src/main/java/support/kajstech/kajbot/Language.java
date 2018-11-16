1160b9960cf5064b876b3d2e5f2fccb93e4791b1
Minemand <Minemand@outlook.dk>
2018-11-16 21:33:22 +0100

    Custom language file option

--- src/main/java/support/kajstech/kajbot/Language.java
+++ src/main/java/support/kajstech/kajbot/Language.java
@@ -1,13 +1,15 @@
 package support.kajstech.kajbot;
 
 
+import support.kajstech.kajbot.utils.ConfigManager;
+
 import java.io.*;
 import java.util.Properties;
 
 public class Language {
 
     public static Properties messages = new Properties();
-    private static File file = new File("lang.properties");
+    private static File file = new File(ConfigManager.getProperty("Language") + ".properties");
 
     static void run() {
 
@@ -15,7 +17,7 @@ public class Language {
             messages.load(new FileInputStream(file));
         } catch (IOException e) {
             try {
-                InputStream in = ClassLoader.getSystemResourceAsStream("lang.properties");
+                InputStream in = ClassLoader.getSystemResourceAsStream("en_US.properties");
                 byte[] buffer = new byte[in.available()];
                 OutputStream out = new FileOutputStream(file);
                 in.read(buffer);
