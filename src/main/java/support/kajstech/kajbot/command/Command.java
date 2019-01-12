844e96cd623b97e8ee52c283563e9c33e77e3222
Minemand <Minemand@outlook.dk>
2019-01-12 14:06:02 +0100

    quick fix

--- src/main/java/support/kajstech/kajbot/command/Command.java
+++ src/main/java/support/kajstech/kajbot/command/Command.java
@@ -23,10 +23,8 @@ public abstract class Command {
 
         if (requiredRole != null) {
             if (!event.getEvent().isFromType(ChannelType.TEXT) || event.getEvent().getMember().getRoles().stream().noneMatch(r -> r.getName().equalsIgnoreCase(requiredRole))) {
-                if(!(event.getEvent().isFromType(ChannelType.PRIVATE) || event.getEvent().isFromType(ChannelType.GROUP))) {
-                    event.getEvent().getChannel().sendMessage((Language.getMessage("CommandSystem.MISSING_ROLE")).replace("%ROLE%", requiredRole)).queue();
-                    return;
-                }
+                event.getEvent().getChannel().sendMessage((Language.getMessage("CommandSystem.MISSING_ROLE")).replace("%ROLE%", requiredRole)).queue();
+                return;
             }
         }
 
