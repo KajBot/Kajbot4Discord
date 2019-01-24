cd04893b0c23f0d6fd2d7f66fa40c9fd63efcca4
Minemand <Minemand@outlook.dk>
2019-01-24 20:02:06 +0100

    Update Clear.java

--- src/main/java/support/kajstech/kajbot/command/commands/Clear.java
+++ src/main/java/support/kajstech/kajbot/command/commands/Clear.java
@@ -21,11 +21,33 @@ public class Clear extends Command {
     public void execute(CommandEvent e) {
         try {
             int AMOUNT = Integer.parseInt(e.getArgsSplit().get(0));
-            MessageHistory history = new MessageHistory(e.getChannel());
-            List<Message> msgs;
-            msgs = history.retrievePast(AMOUNT).complete();
-            e.getChannel().deleteMessages(msgs).queue();
-            e.reply((Language.getMessage("Clear.SUCCESS")).replace("%AMOUNT%", e.getArgsSplit().get(0)));
+            if (AMOUNT > 100) {
+                int _100s = AMOUNT / 100;
+                int _1s = AMOUNT - _100s * 100;
+                int _AMOUNT = AMOUNT;
+                for (int i = 0; i < _100s + 1; ++i) {
+                    if (_AMOUNT >= 100) {
+                        _AMOUNT = _AMOUNT - 100;
+                        MessageHistory history = new MessageHistory(e.getChannel());
+                        List<Message> msgs;
+                        msgs = history.retrievePast(100).complete();
+                        e.getChannel().deleteMessages(msgs).queue();
+                    } else if (_1s > 0) {
+                        MessageHistory history = new MessageHistory(e.getChannel());
+                        List<Message> msgs;
+                        msgs = history.retrievePast(_1s).complete();
+                        e.getChannel().deleteMessages(msgs).queue();
+                    }
+
+                }
+                e.reply((Language.getMessage("Clear.SUCCESS")).replace("%AMOUNT%", e.getArgsSplit().get(0)));
+            } else {
+                MessageHistory history = new MessageHistory(e.getChannel());
+                List<Message> msgs;
+                msgs = history.retrievePast(AMOUNT).complete();
+                e.getChannel().deleteMessages(msgs).queue();
+                e.reply((Language.getMessage("Clear.SUCCESS")).replace("%AMOUNT%", e.getArgsSplit().get(0)));
+            }
         } catch (Exception ex) {
             LogHelper.error(this.getClass(), ex, e.getMessage().getContentRaw());
             e.reply(Language.getMessage("Clear.ERROR"));
