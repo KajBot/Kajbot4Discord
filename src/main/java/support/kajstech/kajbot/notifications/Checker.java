f6531440b6df75e0aff157001a1b2725b75f3679
DumFroe <Minemand@outlook.dk>
2019-05-24 17:04:37 +0200

    Summary (required)

--- src/main/java/support/kajstech/kajbot/notifications/Checker.java
+++ src/main/java/support/kajstech/kajbot/notifications/Checker.java
@@ -1,6 +1,7 @@
 package support.kajstech.kajbot.notifications;
 
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import support.kajstech.kajbot.utils.Config;
 import support.kajstech.kajbot.utils.LogHelper;
 
 import java.io.IOException;
@@ -8,15 +9,25 @@ import java.io.IOException;
 public class Checker extends ListenerAdapter {
 
     public static void run() throws InterruptedException {
-
-        //SITES
-        try {
-            Twitch.check();
-            YouTube.check();
-        } catch (IOException e) {
-            LogHelper.error(Checker.class, e.toString());
+        while (true) {
+            //SITES
+            try {
+                if (Config.cfg.contains("Twitch client ID") && Config.cfg.contains("Twitch channels") && Config.cfg.get("Twitch live notifications").equalsIgnoreCase("true")) {
+                    Twitch.check();
+                }
+                if (Config.cfg.contains("YouTube API key") && Config.cfg.contains("YouTube channels")) {
+                    if (Config.cfg.get("YouTube video notifications").equalsIgnoreCase("true")) {
+                        YouTubeVideo.check();
+                    }
+                    if (Config.cfg.get("YouTube live notifications").equalsIgnoreCase("true")) {
+                        YouTubeLive.check();
+                    }
+                }
+            } catch (IOException e) {
+                LogHelper.error(Checker.class, e.toString());
+            }
+            Thread.sleep(6000);
         }
 
-        Thread.sleep(60000);
     }
 }
