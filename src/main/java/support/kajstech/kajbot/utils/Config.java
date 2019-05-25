cd419eec229fdfe4695b82494b35c71888e28cca
DumFroe <Minemand@outlook.dk>
2019-05-25 17:51:34 +0200

    .

--- src/main/java/support/kajstech/kajbot/utils/Config.java
+++ src/main/java/support/kajstech/kajbot/utils/Config.java
@@ -8,9 +8,8 @@ import java.io.File;
 
 public class Config {
 
-    private static File cfgPath = new File(System.getProperty("user.dir") + "/config");
+    private static File cfgPath = new File(System.getProperty("user.dir") + "/config.properties");
     private static File fallbackPath = new File("config.properties");
 
     public static SimpleCfg cfg = new ConfigFactory(cfgPath).format(Format.PROPERTIES).fallback(fallbackPath).build();
-
 }
